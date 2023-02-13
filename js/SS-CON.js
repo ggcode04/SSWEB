4ceb2ed33dcc328cec0ee04df11f8115f14a1ff9
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-02-13 15:31:46 -0600

    TableData

--- js/SS-CON.js
+++ js/SS-CON.js
@@ -23,3 +23,29 @@ function showTabs (activeBtn, contentsId2) {
     activeBtn.classList.add('show2');
     document.getElementById(contentsId2).classList.add('show2');
 }
+
+const table = document.getElementById("table");
+const modal = document.getElementById("modal");
+const inputs = document.querySelectorAll("input");
+let count = 0;
+
+window.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-warning")) {
+    let data = e.target.parentElement.parentElement.children;
+    fillData(data);
+    modal.classList.toggle("translate");
+  }
+
+  if (e.target.matches(".btn-danger")) {
+  modal.classList.toggle("translate");
+  count=0
+  }
+});
+
+const fillData = (data) => {
+  for (let index of inputs) {
+    index.value = data[count].textContent;
+    console.log(index);
+    count += 1;
+  }
+};
