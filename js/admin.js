156071a811b6110a9868c37997193f1beabbfe4a
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-03-03 17:52:54 -0600

    NewData

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,7 @@
 $('#s_empresa').html("Soluciones SS");
+
+$('#business').hide();
+$('#empresa').click(function(){
+    $('#slider').hide();
+    $('#business').show();
+});
---
+++ js/chatbot.js
@@ -0,0 +1,13 @@
+window.tiledeskSettings= 
+{
+    projectid: "64021da64156b00019275f40"
+};
+(function(d, s, id) { 
+    var w=window; var d=document; var i=function(){i.c(arguments);};
+    i.q=[]; i.c=function(args){i.q.push(args);}; w.Tiledesk=i;                    
+    var js, fjs=d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js=d.createElement(s); 
+    js.id=id; js.async=true; js.src="https://widget.tiledesk.com/v5/launch.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document,'script','tiledesk-jssdk'));
