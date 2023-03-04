4588a36e22080922a04f275268b89ef4466b2acf
SolucionesSS <agranados@solucionesss.com>
2023-03-03 18:07:50 -0600

    Actualizacion

--- js/SS-CON.js
+++ js/SS-CON.js
@@ -24,7 +24,27 @@ function showTabs (activeBtn, contentsId2) {
     document.getElementById(contentsId2).classList.add('show2');
 }
 
-$("#tabContents").tabs();
+
+$( function() {
+    var tabs = $("#tabContents").tabs();
+    var previouslyFocused = false;
+ 
+    tabs.find( ".ui-tabs-nav" ).sortable({
+      axis: "x",
+ 
+      // Sortable removes focus, so we need to restore it if the tab was focused
+      // prior to sorting
+      start: function(event, ui) {
+        previouslyFocused = document.activeElement === ui.item[ 0 ];
+      },
+      stop: function(event, ui) {
+        tabs.tabs( "refresh" );
+        if (previouslyFocused) {
+          ui.item.trigger( "focus" );
+        }
+      }
+    });
+} );
 
     /*MENUS DE NAVEGACION*/
     $("#menu_Movi").hide();
@@ -37,6 +57,7 @@ $("#tabContents").tabs();
     /*Activación de los menus*/
     $("#Opcion_Cata").click(function(){
         $("#menu_Cata").show();
+
         $("#menu_Movi").hide();
         $("#menu_Cierre").hide();
         $("#menu_Balances").hide();
@@ -59,6 +80,7 @@ $("#tabContents").tabs();
         $("#menu_Cata").hide();
         $("#menu_Movi").hide();
         $("#menu_Cierre").show();
+
         $("#menu_Balances").hide();
         $("#menu_Consultas").hide();
         $("#menu_Reportes").hide();
@@ -70,6 +92,7 @@ $("#tabContents").tabs();
         $("#menu_Movi").hide();
         $("#menu_Cierre").hide();
         $("#menu_Balances").hide();
+
         $("#menu_Consultas").show();
         $("#menu_Reportes").hide();
         $("#menu_Confi").hide();
@@ -81,6 +104,7 @@ $("#tabContents").tabs();
         $("#menu_Cierre").hide();
         $("#menu_Balances").show();
         $("#menu_Consultas").hide();
+
         $("#menu_Reportes").hide();
         $("#menu_Confi").hide();
     });
@@ -92,6 +116,7 @@ $("#tabContents").tabs();
         $("#menu_Balances").hide();
         $("#menu_Consutlas").hide();
         $("#menu_Reportes").show();
+
         $("#menu_Confi").hide();
     });
 
@@ -107,14 +132,9 @@ $("#tabContents").tabs();
 
 
     /*TITULOS TABS*/
-    $("#t_tab1_1").hide();
     $("#t_tab1_2").hide();
     $("#t_tab1_3").hide();
     $("#t_tab1_4").hide();
-    $("#t_tab1_5").hide();
-    $("#t_tab1_6").hide();
-    $("#t_tab1_7").hide();
-    $("#t_tab1_8").hide();
 
     $("#t_tab2_1").hide();
     $("#t_tab2_2").hide();
@@ -158,6 +178,7 @@ $("#tabContents").tabs();
     /*FUNCIONES CATALOGO*/
     $("#op1_1").click(function(){
         $("#t_tab1_1").show();
+        $("#tabContents").tabs({active: 0});
     })
 
     $("#c1_1").click(function(){
@@ -167,6 +188,7 @@ $("#tabContents").tabs();
 
     $("#op1_2").click(function(){
         $("#t_tab1_2").show();
+        $("#tabContents").tabs({active: 1});
     })
 
     $("#c1_2").click(function(){
@@ -176,6 +198,7 @@ $("#tabContents").tabs();
 
     $("#op1_3").click(function(){
         $("#t_tab1_3").show();
+        $("#tabContents").tabs({active: 2});
     })
 
     $("#c1_3").click(function(){
@@ -185,52 +208,18 @@ $("#tabContents").tabs();
 
     $("#op1_4").click(function(){
         $("#t_tab1_4").show();
+        $("#tabContents").tabs({active: 3});
     })
 
     $("#c1_4").click(function(){
         $("#t_tab1_4").hide();
     })
 
-
-    $("#op1_5").click(function(){
-        $("#t_tab1_5").show();
-    })
-
-    $("#c1_5").click(function(){
-        $("#t_tab1_5").hide();
-    })
-
-
-    $("#op1_6").click(function(){
-        $("#t_tab1_6").show();
-    })
-
-    $("#c1_6").click(function(){
-        $("#t_tab1_6").hide();
-    })
-
-
-    $("#op1_7").click(function(){
-        $("#t_tab1_7").show();
-    })
-
-    $("#c1_7").click(function(){
-        $("#t_tab1_7").hide();
-    })
-
-
-    $("#op1_8").click(function(){
-        $("#t_tab1_8").show();
-    })
-
-    $("#c1_8").click(function(){
-        $("#t_tab1_8").hide();
-    })
-
     /*FUNCIONES MOVIMIENTOS*/
 
     $("#op2_1").click(function(){
         $("#t_tab2_1").show();
+        $("#tabContents").tabs({active: 4});
     })
 
     $("#c2_1").click(function(){
@@ -240,6 +229,7 @@ $("#tabContents").tabs();
 
     $("#op2_2").click(function(){
         $("#t_tab2_2").show();
+        $("#tabContents").tabs({active: 5});
     })
 
     $("#c2_2").click(function(){
@@ -249,6 +239,7 @@ $("#tabContents").tabs();
 
     $("#op2_3").click(function(){
         $("#t_tab2_3").show();
+        $("#tabContents").tabs({active: 6});
     })
 
     $("#c2_3").click(function(){
@@ -258,6 +249,7 @@ $("#tabContents").tabs();
 
     $("#op2_4").click(function(){
         $("#t_tab2_4").show();
+        $("#tabContents").tabs({active: 7});
     })
 
     $("#c2_4").click(function(){
@@ -267,6 +259,7 @@ $("#tabContents").tabs();
 
     $("#op2_5").click(function(){
         $("#t_tab2_5").show();
+        $("#tabContents").tabs({active: 8});
     })
 
     $("#c2_5").click(function(){
@@ -276,6 +269,7 @@ $("#tabContents").tabs();
 
     $("#op2_6").click(function(){
         $("#t_tab2_6").show();
+        $("#tabContents").tabs({active: 9});
     })
 
     $("#c2_6").click(function(){
@@ -286,6 +280,7 @@ $("#tabContents").tabs();
 
     $("#op3_1").click(function(){
         $("#t_tab3_1").show();
+        $("#tabContents").tabs({active: 10});
     })
 
     $("#c3_1").click(function(){
@@ -295,6 +290,7 @@ $("#tabContents").tabs();
 
     $("#op3_2").click(function(){
         $("#t_tab3_2").show();
+        $("#tabContents").tabs({active: 11});
     })
 
     $("#c3_2").click(function(){
@@ -304,6 +300,7 @@ $("#tabContents").tabs();
 
     $("#op3_3").click(function(){
         $("#t_tab3_3").show();
+        $("#tabContents").tabs({active: 12});
     })
 
     $("#c3_3").click(function(){
@@ -315,6 +312,7 @@ $("#tabContents").tabs();
 
     $("#op4_1").click(function(){
         $("#t_tab4_1").show();
+        $("#tabContents").tabs({active: 13});
     })
 
     $("#c4_1").click(function(){
@@ -324,6 +322,7 @@ $("#tabContents").tabs();
 
     $("#op4_2").click(function(){
         $("#t_tab4_2").show();
+        $("#tabContents").tabs({active: 14});
     })
 
     $("#c4_2").click(function(){
@@ -333,6 +332,7 @@ $("#tabContents").tabs();
 
     $("#op4_3").click(function(){
         $("#t_tab4_3").show();
+        $("#tabContents").tabs({active: 15});
     })
 
     $("#c4_3").click(function(){
@@ -342,6 +342,7 @@ $("#tabContents").tabs();
 
     $("#op4_4").click(function(){
         $("#t_tab4_4").show();
+        $("#tabContents").tabs({active: 16});
     })
 
     $("#c4_4").click(function(){
@@ -351,6 +352,7 @@ $("#tabContents").tabs();
 
     $("#op4_5").click(function(){
         $("#t_tab4_5").show();
+        $("#tabContents").tabs({active: 17});
     })
 
     $("#c4_5").click(function(){
@@ -360,6 +362,7 @@ $("#tabContents").tabs();
 
     $("#op4_6").click(function(){
         $("#t_tab4_6").show();
+        $("#tabContents").tabs({active: 18});
     })
 
     $("#c4_6").click(function(){
@@ -371,6 +374,7 @@ $("#tabContents").tabs();
 
     $("#op5_1").click(function(){
         $("#t_tab5_1").show();
+        $("#tabContents").tabs({active: 19});
     })
 
     $("#c5_1").click(function(){
@@ -380,6 +384,7 @@ $("#tabContents").tabs();
 
     $("#op5_2").click(function(){
         $("#t_tab5_2").show();
+        $("#tabContents").tabs({active: 20});
     })
 
     $("#c5_2").click(function(){
@@ -389,6 +394,7 @@ $("#tabContents").tabs();
 
     $("#op5_3").click(function(){
         $("#t_tab5_3").show();
+        $("#tabContents").tabs({active: 21});
     })
 
     $("#c5_3").click(function(){
@@ -398,6 +404,7 @@ $("#tabContents").tabs();
 
     $("#op5_4").click(function(){
         $("#t_tab5_4").show();
+        $("#tabContents").tabs({active: 22});
     })
 
     $("#c5_4").click(function(){
@@ -407,6 +414,7 @@ $("#tabContents").tabs();
     /*FUNCIONES REPORTES*/
     $("#op6_1").click(function(){
         $("#t_tab6_1").show();
+        $("#tabContents").tabs({active: 23});
     })
 
     $("#c6_1").click(function(){
@@ -416,6 +424,7 @@ $("#tabContents").tabs();
 
     $("#op6_2").click(function(){
         $("#t_tab6_2").show();
+        $("#tabContents").tabs({active: 24});
     })
 
     $("#c6_2").click(function(){
@@ -425,6 +434,7 @@ $("#tabContents").tabs();
 
     $("#op6_3").click(function(){
         $("#t_tab6_3").show();
+        $("#tabContents").tabs({active: 25});
     })
 
     $("#c6_3").click(function(){
@@ -434,6 +444,7 @@ $("#tabContents").tabs();
 
     $("#op6_4").click(function(){
         $("#t_tab6_4").show();
+        $("#tabContents").tabs({active: 26});
     })
 
     $("#c6_4").click(function(){
@@ -443,6 +454,7 @@ $("#tabContents").tabs();
 
     $("#op6_5").click(function(){
         $("#t_tab6_5").show();
+        $("#tabContents").tabs({active: 27});
     })
 
     $("#c6_5").click(function(){
@@ -452,6 +464,7 @@ $("#tabContents").tabs();
 
     $("#op6_6").click(function(){
         $("#t_tab6_6").show();
+        $("#tabContents").tabs({active: 28});
     })
 
     $("#c6_6").click(function(){
@@ -461,6 +474,7 @@ $("#tabContents").tabs();
 
     $("#op6_7").click(function(){
         $("#t_tab6_7").show();
+        $("#tabContents").tabs({active: 29});
     })
 
     $("#c6_7").click(function(){
@@ -470,6 +484,7 @@ $("#tabContents").tabs();
     /*FUNCIONES CONFIGURACION*/
     $("#op7_1").click(function(){
         $("#t_tab7_1").show();
+        $("#tabContents").tabs({active: 30});
     })
 
     $("#c7_1").click(function(){
@@ -479,6 +494,7 @@ $("#tabContents").tabs();
 
     $("#op7_2").click(function(){
         $("#t_tab7_2").show();
+        $("#tabContents").tabs({active: 31});
     })
 
     $("#c7_2").click(function(){
@@ -488,6 +504,7 @@ $("#tabContents").tabs();
 
     $("#op7_3").click(function(){
         $("#t_tab7_3").show();
+        $("#tabContents").tabs({active: 32});
     })
 
     $("#c7_3").click(function(){
@@ -497,6 +514,7 @@ $("#tabContents").tabs();
 
     $("#op7_4").click(function(){
         $("#t_tab7_4").show();
+        $("#tabContents").tabs({active: 33});
     })
 
     $("#c7_4").click(function(){
@@ -506,6 +524,7 @@ $("#tabContents").tabs();
 
     $("#op7_5").click(function(){
         $("#t_tab7_5").show();
+        $("#tabContents").tabs({active: 34});
     })
 
     $("#c7_5").click(function(){
