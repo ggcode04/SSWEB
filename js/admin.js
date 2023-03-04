f8f4930cd1e081cd0b0c3270bc3eaa184f34d7d2
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-03-03 18:34:04 -0600

    NewData

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,9 @@
 $('#s_empresa').html("Soluciones SS");
+
+$('#s_empresa').html("Soluciones SS");
+
+$('#business').hide();
+$('#empresa').click(function(){
+    $('#slider').hide();
+    $('#business').show();
+});
