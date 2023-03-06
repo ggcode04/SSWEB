bbe1d7d61e661c6374c91ac2750e65c73ebdf085
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-03-06 16:14:03 -0600

    NewResolutions & data

--- js/admin.js
+++ js/admin.js
@@ -3,7 +3,15 @@ $('#s_empresa').html("Soluciones SS");
 $('#s_empresa').html("Soluciones SS");
 
 $('#business').hide();
+$('#business2').hide();
 $('#empresa').click(function(){
     $('#slider').hide();
     $('#business').show();
+    $('#business2').show();
 });
+
+const add = document.getElementById('add');
+
+add.addEventListener('click', () => {
+    alert('Hola!')
+})
